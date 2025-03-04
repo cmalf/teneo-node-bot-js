e35502395f8ee85bfbcc3badc926e32a94d08115
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2025-03-04 09:44:55 +0700

    Create DataAllAccount.js for Manually Input Data Bearer Token!

---
+++ DataAllAccount.js
@@ -0,0 +1,14 @@
+// if you want to enter Bearer Token data manually.
+// If you have already logged in with a script, replace this file with the DataAllAccount.js file from the Login Folder.
+const DataAllAccount = [
+  {
+    "email": "xxxxxx",
+    "access_token": "xxxxxx"
+  },
+  {
+    "email": "xxxxxx",
+    "access_token": "xxxxxx"
+  }
+];
+
+module.exports = { DataAllAccount };
